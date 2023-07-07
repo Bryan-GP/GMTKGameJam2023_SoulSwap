39222437f5802f8fa85208ffff10c928d71e4f10
ChalkBox-1 <chalk.box1@outlook.com>
2023-07-08 00:45:16 +0100

    Update Soul.cs

--- Assets/Soul.cs
+++ Assets/Soul.cs
@@ -16,16 +16,11 @@ public class Soul : MonoBehaviour
     public GameObject[] enemies_in_range2;
     public GameObject chosenEnemy;
     public GameObject preivousSelf;
-  
-
-
- 
 
     //soul swap:
     //while holding down a button specifyed to soul swap  --y
     //choose an enemy that you want to soul swap to, they cant swap to a soul which is of the same colour, and they can change to a body of any colour when just a souly  --n
     //when swapped this script should be impelemted into thier object which overrides their own script --n
-  
     public void getEnemies()
     {
         if (swapping)
