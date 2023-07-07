eb64aa03d917e0ecef21090b70a02bf15d49dab3
Joshua <Joshreyes@gmail.com>
2023-07-07 21:03:13 +0100

    added bugged swapscript

---
+++ Assets/Swapscripts.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swapscripts : MonoBehaviour
+{
+    public GameObject chosenentity = null;
+    public GameObject preivousentity = null;
+
+    // this script will take in the gameobject and switch its selected scripts
+    //still need to add ai script
+    public void addsoulscript()
+    {
+        //Destroy(Soul)
+        //chosenentity.AddComponent<ai>();
+        //chosenentity.AddComponent<Soul>();
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
