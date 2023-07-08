e032ae5f593e4995e52905638022d8a3a40b46a1
Joshua <Joshreyes@gmail.com>
2023-07-09 00:56:35 +0100

    finished title screen

--- Assets/Scenemanagment.cs
+++ Assets/Scenemanagment.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scenemanagment : MonoBehaviour
 {
-   
+    public void GotoScene(string Scenename)
+    {
+        SceneManager.LoadScene(Scenename);
+    }
 }
