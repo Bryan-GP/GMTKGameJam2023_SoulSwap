c761b74fc2e434ab3038c6fb13487fd7d04c96eb
ChalkBox-1 <chalk.box1@outlook.com>
2023-07-08 21:03:40 +0100

    idk whats going on rn

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 
-    public float movespeed = 5f;
+    public float movespeed = 1f;
+    public float jumpspeed = 8f;
     public Rigidbody2D rb;
+    //Transform ;
     public Animator animator;
+    
+    public bool isJumping;
 
     Vector2 movement;
 
@@ -16,16 +20,37 @@ public class PlayerMove : MonoBehaviour
     {
         //inputs
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        //
+        if (Input.GetButtonDown("Jump") && !isJumping )
+        {
+            movement.y = Input.GetAxisRaw("Jump") * jumpspeed * Time.deltaTime;
+        }
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
+        
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isJumping = true;
+        }
 
     }
 
     void FixedUpdate()
     {
         //movement
-        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+        rb.AddForce(rb.position + movement * movespeed);
     }
 }
