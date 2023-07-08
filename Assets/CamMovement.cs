f5710af36a1a9b61585fc7191252d2a3b3a40c01
Joshua <Joshreyes@gmail.com>
2023-07-08 12:37:35 +0100

    Update CamMovement.cs

--- Assets/CamMovement.cs
+++ Assets/CamMovement.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float camspeed = 2f;
-    public Transform target;
+    public Transform target = null;
+    public GameObject targetobj = null;
 
 
-
+    private void Start()
+    {
+        targetobj = GameObject.FindGameObjectWithTag("Soul");
+        target = targetobj.GetComponent<Transform>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        targetobj = GameObject.FindGameObjectWithTag("Soul");
+        target = targetobj.GetComponent<Transform>();
+
         Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
 
         transform.position = Vector3.Slerp(transform.position, newPos, camspeed * Time.deltaTime);
