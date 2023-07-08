b2420e9d3e96a16cd5863c13a9a6c49ec3560491
ChalkBox-1 <chalk.box1@outlook.com>
2023-07-08 02:56:58 +0100

    update
    idk man it has truss me bro

--- Assets/swapradius.cs
+++ Assets/swapradius.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class swapradius : MonoBehaviour
     public GameObject[] enemyList;
     public List<GameObject> enemies_in_range;
     public Collider2D radius;
+    public int index = 0; 
 
     
       
@@ -17,7 +19,18 @@ public class swapradius : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.timeScale == 0.05f)
+        {
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                index -= 1;
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                index += 1;
+            }
+        }
+
     }
 
     void OnTriggerEnter2D(Collider2D collision)
