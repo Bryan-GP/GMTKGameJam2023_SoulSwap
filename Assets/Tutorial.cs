ff7d3703f5e300b9d9410099da7b53f4a323a508
Joshua <Joshreyes@gmail.com>
2023-07-09 03:42:39 +0100

    pollished again

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -24,7 +24,7 @@ public class Tutorial : MonoBehaviour
         }
 
        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && showfirst == true)
         {
             showfirst = false;
             tagslist[0].SetActive(true);
