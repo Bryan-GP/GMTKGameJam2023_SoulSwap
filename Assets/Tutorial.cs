79debf6077e1327cbe382f5f993ae093129dc7dc
Joshua <Joshreyes@gmail.com>
2023-07-09 03:38:51 +0100

    pollished tutourial

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -11,7 +11,7 @@ public class Tutorial : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tagslist[index].SetActive(true);
+        
     }
 
     // Update is called once per frame
